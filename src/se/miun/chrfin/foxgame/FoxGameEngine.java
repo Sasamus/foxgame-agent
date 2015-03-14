d11eab3f89062e391f9c6c99ee1519cb34716f4e
Sasamus <labus93@gmail.com>
2015-03-14 17:29:16 +0100

    Implemented holding of the boards state

--- src/se/miun/chrfin/foxgame/FoxGameEngine.java
+++ src/se/miun/chrfin/foxgame/FoxGameEngine.java
@@ -1,5 +1,7 @@
 package se.miun.chrfin.foxgame;
 
+import java.util.ArrayList;
+
 import se.miun.chrfin.foxgame.com.GameStatus;
 import se.miun.chrfin.foxgame.setup.PlayerSetup;
 
@@ -8,28 +10,48 @@ import se.miun.chrfin.foxgame.setup.PlayerSetup;
  */
 public class FoxGameEngine implements AiGameEngine {
 
+	// TODO: Better name?
 	/**
-	 * Holds a PlayerSetup object
+	 * Holds the player name
 	 */
-	private final PlayerSetup setup;
-	
+	private final String playerName = "alen1200";
+
 	/**
 	 * Holds a String representing the players role
 	 */
 	private final String playerRole;
 
+	private ArrayList<ArrayList<Integer>> gameBoard = new ArrayList<ArrayList<Integer>>();
+
 	/**
 	 * Constructor
 	 * 
-	 * @param setup A PlayerSetuo object holding the setup information
+	 * @param setup
+	 *            A PlayerSetuo object holding the setup information
 	 */
 	public FoxGameEngine(PlayerSetup setup) {
-		
-		// Set setup
-		this.setup = setup;
-		
+
 		// Set playerRole
 		playerRole = setup.playerRole;
+
+		// Iterates through and creates the game board and sets the fields to
+		// the appropriate values
+		// 0 = empty, 1 = sheep, 2 = fox
+		for (int x = 1; x <= 7; x++) {
+
+			gameBoard.add(new ArrayList<Integer>());
+
+			for (int y = 1; y <= 7; y++) {
+
+				if (y == 1 && (x == 3 || x == 5)) {
+					gameBoard.get(x).set(y, 2);
+				} else if (y >= 4) {
+					gameBoard.get(x).set(y, 1);
+				} else {
+					gameBoard.get(x).set(y, 0);
+				}
+			}
+		}
 	}
 
 	/**
@@ -37,17 +59,16 @@ public class FoxGameEngine implements AiGameEngine {
 	 */
 	@Override
 	public String getMove(GameStatus status) {
-		
+
 		// Acts depending on if playerRole equals FOX or SHEEP
-		if(playerRole.equals("FOX")){
-			
+		if (playerRole.equals("FOX")) {
+
 		} else {
-			
+
 		}
-		
+
 		return null;
 	}
-	
 
 	@Override
 	public void updateState(String move) {
@@ -56,6 +77,6 @@ public class FoxGameEngine implements AiGameEngine {
 
 	@Override
 	public String getPlayerName() {
-		return setup.playerName;
+		return playerName;
 	}
 }
