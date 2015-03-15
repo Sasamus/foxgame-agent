cbe3134e8197abfa3d9c0f1943630184d18a032a
Sasamus <labus93@gmail.com>
2015-03-15 14:53:13 +0100

    Continued work on the getSuccessors method

--- src/se/miun/chrfin/foxgame/FoxGameEngine.java
+++ src/se/miun/chrfin/foxgame/FoxGameEngine.java
@@ -2,11 +2,11 @@ package se.miun.chrfin.foxgame;
 
 import java.util.ArrayList;
 
-import ae.foxgameagent.Board;
 import se.miun.chrfin.foxgame.com.GameStatus;
 import se.miun.chrfin.foxgame.logic.Move;
 import se.miun.chrfin.foxgame.logic.Position;
 import se.miun.chrfin.foxgame.setup.PlayerSetup;
+import ae.foxgameagent.Board;
 
 /**
  * @author Christoffer Fink
@@ -16,8 +16,15 @@ public class FoxGameEngine implements AiGameEngine {
 	// TODO: See if there are faster variables/collections to use.
 
 	// TODO: When are foxed removed exactly?
-	
-	// TODO: Generate successors once, possibly in the constructor, and store them instead of making new ones every time.
+
+	// TODO: Generate successors once, possibly in the constructor, and store
+	// them instead of making new ones every time.
+
+	// TODO: Account for time
+
+	// TODO: Get a move
+
+	// TODO: Generate Successors
 
 	// TODO: Better player name?
 	/**
@@ -34,7 +41,7 @@ public class FoxGameEngine implements AiGameEngine {
 	 * Holds the Board
 	 */
 	private Board board = new Board();
-	
+
 	/**
 	 * Constructor
 	 * 
@@ -74,37 +81,61 @@ public class FoxGameEngine implements AiGameEngine {
 	 *         foxes and sheep
 	 */
 	private ArrayList<ArrayList<Position>> getSuccessors() {
-		
+
 		// Holds the Successors
-		ArrayList<ArrayList<Position>> successors = new ArrayList<ArrayList<Position>>();
-		
-		// Add the current positions to successors
-		successors.add(foxPositions.);
-		successors.add(sheepPositions);
+		ArrayList<Board> successors = new ArrayList<Board>();
+
+		// Holds a successor
+		Board successor;
+
+		// Holds the new Positions
+		ArrayList<Position> newPositions = new ArrayList<Position>();
 
 		// Iterate through sheepPositions
-		for (Position sheepPosition : sheepPositions) {
+		for (Position sheepPosition : board.getSheepPositions()) {
+
+			// Set successor to match board
+			successor = new Board(board);
 
 			// Get the x and y values of foxPosition
-			int x = sheepPosition.x;
-			int y = sheepPosition.y;
+			int x = sheepPosition.getX();
+			int y = sheepPosition.getY();
 
 			// Look for possible horizontal moves
-			
-			// If the sheep is in the 6 top positions
-			if(y == 1 || y == 2){
-				
-				// If the sheep is at the left edge of those positions 
-				if(x == 3){
-					Position position = new Position(x + 1, y);
-					switch (isOccupied(position)) {
-					case 0:
-						successors.
-						break;
 
-					default:
-						break;
-					}
+			// If the sheep is in the 6 top positions
+			if (y == 1 || y == 2) {
+
+				// Try to add positions depending on where the current position
+				// is
+				if (x == 3) {
+
+					// Set newPosition with a new Position
+					newPositions.add(new Position(x + 1, y));
+
+				} else if (x == 5) {
+
+					newPositions.add(new Position(x - 1, y));
+
+				} else {
+
+					newPositions.add(new Position(x + 1, y));
+					newPositions.add(new Position(x - 1, y));
+
+				}
+			}
+
+			// Iterate through newPositions
+			for (Position newPosition : newPositions) {
+
+				// Add a new Successor if the new Positions is empty
+				switch (isOccupied(newPosition)) {
+
+				case 0:
+					successor.changePostition(sheepPosition, newPosition);
+					successors.add(successor);
+					break;
+
 				}
 			}
 
@@ -112,19 +143,20 @@ public class FoxGameEngine implements AiGameEngine {
 
 		return null;
 	}
-	
+
 	/**
 	 * Check if a position is occupied, and by what
 	 * 
-	 * @param position The Position to check
+	 * @param position
+	 *            The Position to check
 	 * @return 0 if free, 1 if sheep, 2 if fox
 	 */
-	private int isOccupied(Position position){
-		
+	private int isOccupied(Position position) {
+
 		// Check if something is in position and return an int depending on what
-		if(board.getFoxPositions().contains(position)){
+		if (board.getFoxPositions().contains(position)) {
 			return 2;
-		} else if (board.getSheepPositions().contains(position)){
+		} else if (board.getSheepPositions().contains(position)) {
 			return 1;
 		}
 		return 0;
